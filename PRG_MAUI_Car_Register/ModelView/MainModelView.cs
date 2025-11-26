26309aceb73f9f42e4ca0a8b0925cca0494803ab
arvin.shadmand <arvin.shadmand@5CD3147K2Y>
2025-11-26 11:13:58 +0100

    Viewmodel*

---
+++ PRG_MAUI_Car_Register/ModelView/MainModelView.cs
@@ -0,0 +1,351 @@
+﻿using PRG_MAUI_Car_Register.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace PRG_MAUI_Car_Register.ModelView
+{
+    public class MainModelView : INotifyPropertyChanged
+    {
+
+        //List<Vehicle> vehicleList = new List<Vehicle>();
+
+
+        public ObservableCollection<Vehicle> ListViewVehicles { get; set; } = new ObservableCollection<Vehicle>();
+        public ObservableCollection<string> VehicleTypePicker { get; set; } = new ObservableCollection<string> { "Bil", "MC", "Lastbil" };
+
+        public ICommand RegisterCarCommand { get; }
+        public bool CarRegisterOk = false;
+        public bool ValidateOk = false;
+
+        private string register;
+        private string model;
+        private string manufacture;
+        private string yearmodel;
+        private string selectedvehicle;
+
+        Regex regex = new Regex("[@_!#$%^&*()<>?/|}{~:]");
+
+        public string Register
+        {
+            get => register;
+            set
+            {
+                if (register != value)
+                {
+                    register = value;
+                    OnPropertyChanged(nameof(Register));
+
+                }
+            }
+
+        }
+
+        public string ModelCar
+        {
+            get => model;
+            set
+            {
+                if (model != value)
+                {
+                    model = value;
+                    OnPropertyChanged(nameof(ModelCar));
+
+                }
+            }
+
+        }
+
+        public string Manufacture
+        {
+            get => manufacture;
+            set
+            {
+                if (manufacture != value)
+                {
+                    manufacture = value;
+                    OnPropertyChanged(nameof(Manufacture));
+
+                }
+            }
+
+        }
+
+        public string Yearmodel
+        {
+            get => yearmodel;
+            set
+            {
+                if (yearmodel != value)
+                {
+                    yearmodel = value;
+                    OnPropertyChanged(nameof(Yearmodel));
+
+                }
+            }
+
+        }
+
+        public string VehicleSelected
+        {
+            get => selectedvehicle;
+            set
+            {
+                if (selectedvehicle != value)
+                {
+                    selectedvehicle = value;
+                    OnPropertyChanged(nameof(VehicleSelected));
+
+                }
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public MainModelView()
+        {
+            RegisterCarCommand = new Command(AddCarRegister);
+        }
+        private void AddCarRegister()
+        {
+            CheckEmptyRegister();
+            ValidateRegister();
+
+            if (CarRegisterOk == true && ValidateOk == true)
+            {
+                //if (vehicletype.GetType().Name == "Bil") {
+
+                if (VehicleTypePicker != null)
+                {
+                    Debug.WriteLine(selectedvehicle);
+
+
+
+                    if (selectedvehicle == "Bil")
+                    {
+
+                        
+
+                        ListViewVehicles.Add(new Bil(Bil.Type.Bil)
+                        {
+                            RegistrationNumber = Register,
+                            Model = ModelCar,
+                            Manufacturer = Manufacture,
+                            YearModel = Yearmodel,
+
+                        });
+
+                    }
+
+
+                    if (selectedvehicle == "MC")
+                    {
+
+
+
+                        ListViewVehicles.Add(new MC(MC.Type.MC)
+                        {
+                            RegistrationNumber = Register,
+                            Model = ModelCar,
+                            Manufacturer = Manufacture,
+                            YearModel = Yearmodel,
+
+                        });
+
+                    }
+
+
+
+                    if (selectedvehicle == "Lastbil")
+                    {
+
+
+
+                        ListViewVehicles.Add(new Lastbil(Lastbil.Type.Lastbil)
+                        {
+                            RegistrationNumber = Register,
+                            Model = ModelCar,
+                            Manufacturer = Manufacture,
+                            YearModel = Yearmodel,
+
+                        });
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        private void ValidateRegister() //Validerings logik
+        {
+            ValidateOk = true;
+            if (Register.Length == 6)
+            {
+
+
+                for (int i = 0; i < 3; i++)
+                {
+                    if (!char.IsLetter(Register[i]))
+                        ValidateOk = false;
+
+
+                }
+
+
+                for (int i = 3; i < 6; i++)
+                {
+                    if (i < 5)
+                    {
+                        if (!char.IsDigit(Register[i]))
+                            ValidateOk = false;
+
+                    }
+
+                }
+            }
+
+
+
+            else if (register.Length < 6)
+            {
+                {
+                    ValidateOk = false;
+
+                    if (ValidateOk == false)
+                    {
+                        Application.Current?.MainPage?.DisplayAlert("Error", "Ett registreringsnummer måste bestå av exakt 6 tecken, med tre bokstäver följt av två siffror och en siffra eller bokstav.", "Ok");
+
+                    }
+
+                }
+
+            }
+
+            if (ModelCar.Length >= 1)
+            {
+                if (!regex.IsMatch(ModelCar))
+                {
+
+                }
+                else
+
+                {
+                    ValidateOk = false;
+                    Application.Current?.MainPage?.DisplayAlert("Error", "Modell få inte ha orelevanta symboler!", "Ok");
+                }
+
+            }
+
+
+
+
+            if (Yearmodel.Length < 4)
+            {
+                Application.Current?.MainPage?.DisplayAlert("Error", "Årsmodel måste innehålla exakt 4 siffror!", "Ok");
+                ValidateOk = false;
+            }
+            else
+            {
+
+                if (Yearmodel.Length == 4)
+                {
+
+
+                    if (Regex.IsMatch(Yearmodel, "^[0-9]*$"))
+                    {
+                        int parsed = int.Parse(Yearmodel);
+                        if (parsed >= 1985)
+                        {
+
+                            //Ok!
+                        }
+                        else {
+                            Application.Current?.MainPage?.DisplayAlert("Error", "Det måste vara efter 1985!", "Ok");
+                            ValidateOk = false;
+                        }
+                    }
+                   
+
+
+
+                }
+
+
+
+   
+        }
+
+
+
+        Debug.WriteLine("Validation check:" + ValidateOk);
+
+
+        }
+
+
+
+
+        private void CheckEmptyRegister()
+        {
+            CarRegisterOk = true;
+            if (string.IsNullOrEmpty(selectedvehicle))
+            {
+                CarRegisterOk = false;
+                Application.Current?.MainPage?.DisplayAlert("Error", "Välj en bil typ!", "Ok");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Register))
+            {
+                CarRegisterOk = false;
+                Application.Current?.MainPage?.DisplayAlert("Error", "Register ska ha 3 bokstäver och 3 siffror!", "Ok");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Manufacture))
+            {
+                CarRegisterOk = false;
+                Application.Current?.MainPage?.DisplayAlert("Error", "Tillverkare måste fyllas in!", "Ok");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(ModelCar))
+            {
+                CarRegisterOk = false;
+                Application.Current?.MainPage?.DisplayAlert("Error", "Modellen måste fyllas in!", "Ok");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Yearmodel))
+            {
+                CarRegisterOk = false;
+                Application.Current?.MainPage?.DisplayAlert("Error", "Årsmodellen ska innehålla 4 siffor!", "Ok");
+                return;
+            }
+
+
+        }
+
+
+
+      
+
+
+    }
+
+
+    }
--- PRG_MAUI_Car_Register/View/MainPage.xaml.cs
+++ PRG_MAUI_Car_Register/View/MainPage.xaml.cs
@@ -1,174 +1,21 @@
 ﻿using PRG_MAUI_Car_Register.Model;
+using System.Collections.ObjectModel;
+using  PRG_MAUI_Car_Register.ModelView;
 
 namespace PRG_MAUI_Car_Register
 {
     public partial class MainPage : ContentPage
     {
-        List<Vehicle> vehicleList = new List<Vehicle>();
+
+        //public ObservableCollection<Vehicle> Vehicles { get; set; } = new()
+        //{
+           
+        //}
 
         public MainPage()
         {
             InitializeComponent();
-            pickerType.SelectedIndex = 0;
-        }
-
-
-        private void CheckEmptyRegister()
-        {
-            if (string.IsNullOrEmpty(entryRegistrationNumber.Text))
-            {
-
-                throw new ArgumentException("Inkorret registreringsnummer: De första tre tecknen måste vara bokstäver och de tre andra siffror.");
-
-            }
-
-            if (string.IsNullOrEmpty(entryManufacturer.Text))
-            {
-                throw new ArgumentException("Inkorrekt Tillverkare: Måste innehålla bokstäver av tillverkare");
-            }
-
-            if (string.IsNullOrEmpty(entryModel.Text))
-            {
-                throw new ArgumentException("Inkorrekt Modell: Måste innehålla bokstäver av modellen och får inte ha orelevanta symboler");
-            }
-
-            if (string.IsNullOrEmpty(EntryYearModel.Text))
-            {
-                throw new ArgumentException("Inkorrekt Årsmodell: Måste ha exakt 4 siffror");
-            }
-
-
-        }
-
-      
-
-        private void OnRegisterClicked(object sender, EventArgs e)
-        {
-            try
-            {
-
-                var Picked = pickerType.SelectedIndex;
-
-                if (pickerType.SelectedItem.ToString() == "Bil") {
-
-                    Bil vehicle = new Bil((Bil.Type)pickerType.SelectedIndex);
-                    CheckEmptyRegister();
-
-                    string regNr = entryRegistrationNumber.Text;
-                    vehicle.RegistrationNumber = regNr;
-                    vehicle.Manufacturer = entryManufacturer.Text;
-                    vehicle.Model = entryModel.Text;
-                    vehicle.YearModel = EntryYearModel.Text;
-
-                    vehicleList.Add(vehicle);
-                }
-
-                if (pickerType.SelectedItem.ToString() == "MC")
-                {
-                    MC vehicle = new MC((MC.Type)pickerType.SelectedIndex);
-                    CheckEmptyRegister();
-
-
-                    string regNr = entryRegistrationNumber.Text;
-                    vehicle.RegistrationNumber = regNr;
-                    vehicle.Manufacturer = entryManufacturer.Text;
-                    vehicle.Model = entryModel.Text;
-                    vehicle.YearModel = EntryYearModel.Text;
-
-                    vehicleList.Add(vehicle);
-                }
-
-                if (pickerType.SelectedItem.ToString() == "Lastbil")
-                {
-                    Lastbil vehicle = new Lastbil((Lastbil.Type)pickerType.SelectedIndex);
-                    CheckEmptyRegister();
-
-
-                    string regNr = entryRegistrationNumber.Text;
-                    vehicle.RegistrationNumber = regNr;
-                    vehicle.Manufacturer = entryManufacturer.Text;
-                    vehicle.Model = entryModel.Text;
-                    vehicle.YearModel = EntryYearModel.Text;
-
-                    vehicleList.Add(vehicle);
-                }
-
-
-
-                listViewVehicles.ItemsSource = null;
-                listViewVehicles.ItemsSource = vehicleList;
-
-                entryRegistrationNumber.Text = string.Empty;
-                entryManufacturer.Text = string.Empty;
-                entryModel.Text = string.Empty;
-                EntryYearModel.Text = string.Empty;
-
-
-
-
-            }
-            catch (ArgumentException ex)
-            {
-                DisplayAlert("Fel", ex.Message, "OK");
-            }
-        }
-
-        private void OnRadioCheckedChanged(object sender, CheckedChangedEventArgs e)
-        {
-            if (e.Value != true) return;
-
-            // Skapa en filtrerad lista baserat på vilken radioknapp som är vald
-            List<Vehicle> filteredList;
-
-            if (radioCar.IsChecked)
-            {
-                filteredList = vehicleList.OfType<Bil>().Cast<Vehicle>().ToList();
-            }
-            else if (radioMC.IsChecked)
-            {
-                filteredList = vehicleList.OfType<MC>().Cast<Vehicle>().ToList();
-            }
-            else if (radioTruck.IsChecked)
-            {
-                filteredList = vehicleList.OfType<Lastbil>().Cast<Vehicle>().ToList();
-            }
-            else
-            {
-                // Om "Alla" är vald, visa hela listan
-                filteredList = vehicleList;
-            }
-
-            listViewVehicles.ItemsSource = filteredList;
-        }
-
-        private void OnSearchClicked(object sender, EventArgs e)
-        {
-            string searchReg = entrySearchRegistrationNumber.Text?.ToLower();
-            string searchManufactor = entryManufacturer.Text?.ToLower();
-            string searchModel = entryModel.Text?.ToLower();
-            string searchÅrmodel = EntryYearModel.Text?.ToLower();
-
-            if (string.IsNullOrEmpty(searchReg) || string.IsNullOrEmpty(searchManufactor) || string.IsNullOrEmpty(searchModel))
-            {
-                entrySearchRegistrationNumber.Placeholder = "Ange ett registreringsnummer för att söka.";
-                return;
-            }
-
-            var foundVehicle = vehicleList.FirstOrDefault(v => v.RegistrationNumber?.ToLower() == searchReg);
-
-            if (foundVehicle != null)
-            {
-                labelSearchResult.Text = $"Fordon hittat:\n" +
-                                         $"Registreringsnummer: {foundVehicle.RegistrationNumber}\n" +
-                                         $"Tillverkare: {foundVehicle.Manufacturer}\n" +
-                                         $"Modell: {foundVehicle.Model}\n" +
-                                         $"Typ: {foundVehicle.VehicleType}";
-
-            }
-            else
-            {
-                labelSearchResult.Text = "Inget fordon hittades med det registreringsnumret.";
-            }
+            BindingContext = new MainModelView();
         }
 
     }
